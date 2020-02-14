fd3210d52df4e1cec9d0e0853af94db9ec002c2c
RostislavLebedev <61043001+RostislavLebedev@users.noreply.github.com>
2020-02-14 14:06:29 +0100

    initial commit

---
+++ client.java
@@ -0,0 +1,34 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class client
+{
+	public static void main(String [] args) throws IOException
+	{
+		Socket s = new Socket("localhost", 4999);
+
+		System.out.println("ENTER = Abschicken\n\"-q\" zum Beenden");
+		while(true)
+		{
+			// Nutzereinagben einlesen
+			Scanner scan = new Scanner(System.in);
+			System.out.print("\tSie (ENTER = abschicken): ");
+			String eingabeClient = scan.nextLine();
+
+			if (eingabeClient.equals("-q"))
+				break;
+
+			// Nutzereingaben an den Server schicken
+			PrintWriter pr = new PrintWriter(s.getOutputStream());
+			pr.println(eingabeClient);
+			pr.flush();
+
+			// Nachricht vom Server erhalten
+			InputStreamReader in = new InputStreamReader(s.getInputStream());
+			BufferedReader bf = new BufferedReader(in);
+			String str = bf.readLine();
+			System.out.println("\tserver: " + str);
+		}
+	}
+}
---
+++ server.java
@@ -0,0 +1,36 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class server
+{
+	public static void main(String [] args) throws IOException
+	{
+		ServerSocket servSock = new ServerSocket(4999);
+		Socket s = servSock.accept();
+
+		System.out.println("client connected");
+		System.out.println("ENTER = Abschicken\n\"-q\" zum Beenden");
+		while(true)
+		{
+			// Nachricht vom Client empfangen
+			InputStreamReader in = new InputStreamReader(s.getInputStream());
+			BufferedReader bf = new BufferedReader(in);
+			String str = bf.readLine();
+			System.out.println("\tclient: " + str);
+
+			// Nutzereingaben einlesen
+			Scanner scan = new Scanner(System.in);
+			System.out.print("\tSie (Enter = abschicken): ");
+			String eingabeServer = scan.nextLine();
+
+			if (eingabeServer.equals("-q"))
+				break;
+
+			// Nutzereingaben auslesen
+			PrintWriter pr = new PrintWriter(s.getOutputStream());
+			pr.println(eingabeServer);
+			pr.flush();
+		}
+	}
+}
