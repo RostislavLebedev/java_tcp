eaae7590566632054fe2bfa91e82d4ee6cb3c2ed
RostislavLebedev <61043001+RostislavLebedev@users.noreply.github.com>
2020-02-17 16:05:21 +0100

    runCMD.java hinzugefuegt, CMD-Befehle seitens der Servers am Client ausführen und den Output in einer Datei speichern

---
+++ runCMD.java
@@ -0,0 +1,64 @@
+/*
+ * class runCMD 
+ * Die vom Server geschickten CMD-Befhele können am Client ausgeführt werden.
+ */
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+class runCMD
+{
+    private String basedir;
+
+    public runCMD(String basedir)
+    {
+        this.basedir = basedir;
+    }
+
+    public void execCMD(String command) throws InterruptedException
+    {
+    	command += "> cmdOutput.txt";
+        System.out.println("executing command: " + command);
+        Process p = null;
+        try
+        {
+            p = Runtime.getRuntime().exec(
+                    "cmd /c " //Nur unter Windows notwendig!
+                    + command, null, new File(basedir));
+
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+
+        // Ausgabe in der CMD des Clients
+        /*
+        System.out.println("OUTPUT");
+        printStream(p.getInputStream());
+        System.out.println("ERROR-OUTPUT");
+        printStream(p.getErrorStream());
+        */
+    }
+
+    // Ausgabe in der CMD des Clients
+    public void printStream(InputStream stream)
+    {
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(stream));
+        String line = "";
+        try
+        {
+           while ((line = in.readLine()) != null)
+           {
+                System.out.println(line);
+           }
+           in.close();
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
